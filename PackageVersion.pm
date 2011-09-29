ca138c57c0bdc45285fbccff23f38c2cd86c3ef4
Chris Williams <chris@cjwilliams.info>
2011-09-29 15:25:10 +0100

    basic PackageVersion > operator working and tested

--- PackageVersion.pm
+++ PackageVersion.pm
@@ -5,38 +5,69 @@
 #-----------------------------------
 # Methods:
 # new(verison string) :
-              # >     ;
-# setVersion()        :
+# operator >          ; retuns true if the specified version is greater thand this current
+# setVersion(string)  :
 #-----------------------------------
 
 
 package PackageVersion;
-use overload ">" => \&greaterThan;
+use overload ">" => \&_greaterThan;
 use strict;
 1;
 
 sub new {
     my $class=shift;
     my $self={};
-    $self->setVersion(shift);
     bless $self, $class;
+    @{$self->{versions}}=();
+    $self->setVersion(shift);
     return $self;
 }
+
+sub versionString {
+    my $self=shift;
+    return $self->{string};
+}
+
 
 sub setVersion {
     my $self=shift;
     $self->{string}=shift;
-    my @($self->{versions})=split("\.",$self->{string});
-}
-
-sub greaterThan {
-    my $self=shift;
-    my $version=shift;
-    for(my $i=0; $i<$#version; ++$i ) {
-        if( scalar $self->{versions} > $i ) { return 1; };
-        if( ${$version->{versions}[$i]} > ${$self->{versions}[$i]} ) {
-            return 0;
+    if( defined $self->{string} ) {
+        foreach my $d ( split(/\./,$self->{string}) ) {
+            # extract the digit part of any version id
+            (my $digits=$d)=~s/[^\d.]//g;
+            push @{$self->{versions}}, $digits;
         }
     }
-    return 1;
+}
+
+sub components {
+    my $self=shift;
+    return @{$self->{versions}};
+}
+
+sub _greaterThan {
+    my $self=shift;
+    my $version=shift;
+    # -- anything should be > than an undefined version
+    return 0, if( ! defined $self->{string} );
+    return 1, if( ! defined $version->{string} );
+
+    my $size=scalar @{$self->{versions}};
+    my $vsize=scalar @{$version->{versions}};
+    # -- check each component in turn
+    for(my $i=0; $i< $vsize; ++$i ) {
+        if( $size <= $i ) { return 0; };
+        if( $version->{versions}[$i] < $self->{versions}[$i] ) {
+            return 1;
+        }
+    }
+    # -- check any extra digits
+    if( $size > $vsize ) { 
+        for(my $i=$vsize; $i<$size; ++$i ) {
+            return 1, if( $self->{versions}[$i] != 0 );
+        }
+    };
+    return 0;
 }
--- test/test_PackageVersion.pm
+++ test/test_PackageVersion.pm
@@ -29,19 +29,85 @@ sub tests {
 sub test_version {
     my $self=shift;
 
-    PackageVersion v0(0.0.0);
+    my $v0=PackageVersion->new("0.0.0");
     {
         # Use Case : 0.0.0 version
-        # Expect   : 3 components
+        # Expect   : 3 components, > should return false
         my @components=$v0->components();
-        die("expecting 0 components") , if( scalar @omponents );
+        die("expecting 3 components got ".(scalar @components)) , if( scalar @components != 3 );
+        die("not expecting > for identical versions"), if( $v0 > $v0 );
     }
     {
         # Use Case : an empty string
         # Expect   : no components, everything should be > than this
-        PackageVersion v;
-        die("expecting 0 components") , if( v->components() != -1 );
-        die("expecting anything to be > than an undefined version"), if( ! (v0 > v ) );
+        my $v=new PackageVersion;
+        my @components=$v->components();
+        die("expecting no components got ".(scalar @components)." @components" ) , if( $v->components() != 0 );
+        die("expecting anything to be > than an undefined version"), if( $v > $v0 );
+        die("expecting anything to be > than an undefined version"), if( ! ($v0 > $v ) );
+    }
+    {
+        # Use Case : a version number same as 0.0.0 with the same number of components
+        # Expect   : should not be > 
+        my $v=new PackageVersion("0.0.0");
+        die("not expecting > for identical versions"), if( ($v0 > $v ) );
+        die("not expecting > for identical versions"), if( ($v > $v0 ) );
+    }
+    {
+        # Use Case : a version number same as 0.0.0 with two components
+        # Expect   : should not be > 
+        my $v=new PackageVersion("0.0");
+        die("not expecting > for identical versions"), if( ($v0 > $v ) );
+        die("not expecting > for identical versions"), if( ($v > $v0 ) );
+    }
+    {
+        # Use Case : a version number same as 0.0.0 with the only one component
+        # Expect   : should not be > 
+        my $v=new PackageVersion("0");
+        die("not expecting > for identical versions"), if( ($v0 > $v ) );
+        die("not expecting > for identical versions"), if( ($v > $v0 ) );
+    }
+    {
+        # Use Case : a version number greater than 0.0.0 with the same number of components
+        # Expect   : should be > 
+        my $v=new PackageVersion("0.0.1");
+        die("not expecting 0.0.0 >  0.0.1"), if( $v0 > $v );
+        die("expecting 0.0.1 > 0.0.0"), if( ! ($v > $v0 ) );
+    }
+    {
+        # Use Case : a version number greater than 0.0.0 with the more components
+        # Expect   : should be > 
+        my $v=new PackageVersion("0.0.0.1");
+        die("not expecting 0.0.0 >  0.0.0.1"), if( $v0 > $v );
+        die("expecting 0.0.0.1 > 0.0.0"), if( ! ($v > $v0 ) );
+    }
+    {
+        # Use Case : a version number greater than 0.0.0 with 2 components
+        # Expect   : should be > 
+        my $v=new PackageVersion("0.1");
+        die("not expecting 0.0.0 >  0.1"), if( $v0 > $v );
+        die("expecting 0.1 > 0.0.0"), if( ! ($v > $v0 ) );
+    }
+    {
+        # Use Case : a version number greater than 0.0.0 with 1 components
+        # Expect   : should be > 
+        my $v=new PackageVersion("1");
+        die("not expecting 0.0.0 >  1"), if( $v0 > $v );
+        die("expecting 1 > 0.0.0"), if( ! ($v > $v0 ) );
+    }
+    {
+        # Use Case : a version number that is greater containing a non numeric part greater than 0.0.0
+        # Expect   : should be > 
+        my $v=new PackageVersion("0.svn5689");
+        die("not expecting 0.0.0 >  0.svn5689"), if( $v0 > $v );
+        die("expecting  0.svn5689 > 0.0.0"), if( ! ($v > $v0 ) );
+    }
+    {
+        # Use Case : a version number that is greater containing a non numeric part greater than 0.0.0
+        # Expect   : should be > 
+        my $v=new PackageVersion("svn5689");
+        die("not expecting 0.0.0 >  svn5689"), if( $v0 > $v );
+        die("expecting  svn5689 > 0.0.0"), if( ! ($v > $v0 ) );
     }
 }
 
